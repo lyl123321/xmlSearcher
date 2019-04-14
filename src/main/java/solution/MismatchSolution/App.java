1e22afbe0f7905e712ae3dbeb29d299a283e5e89
lyl123321 <2099367442@qq.com>
2019-04-14 22:39:11 +0800

    dblp

--- src/main/java/solution/MismatchSolution/App.java
+++ src/main/java/solution/MismatchSolution/App.java
@@ -2,6 +2,7 @@ package solution.MismatchSolution;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,7 @@ public class App {
         //阈值 τ
         double τ = 0.9;
         //url
-        String xml = "data/dblp/dblp2.xml"; 
+        String xml = "data/dblp/dblp5.xml"; 
         
         /*
         //1、解析
@@ -22,10 +23,11 @@ public class App {
         */
         
         //2、关键字查询
+        Date date1 = new Date();
         System.out.println("1. Search: ");
-        String[] query = {"Xu", "2000", "Text", "TREC"};
+        String[] query = {"Tomoko", "Ohara", "2001"};
         //获取前 K 个结果
-        int K = 5;
+        int K = 10;
         List<Map> results = Searcher.search(query, xml, K);
         System.out.println("query: ");
         System.out.println(Arrays.toString(query));
@@ -33,17 +35,24 @@ public class App {
         for (Map result : results) {
         	System.out.println("vlca: " + result.get("vlca") + ", nodes: " + Arrays.toString((String[])result.get("nodes")));
 		}
+        Date date2 = new Date();
+        System.out.println("search time: " + (date2.getTime() - date1.getTime()) + "ms");
         
         //3、解决查询的失配问题
         System.out.println("--------------------------------------------------");
         System.out.println("2. Resolve MisMatch Problem: ");
         Resolver resolver = new Resolver(query, results, τ);
         ArrayList<HashMap> suggestedQueries = resolver.resolve();
+        resolver.close();
         if(suggestedQueries != null) {
-            for (HashMap sugQuery : suggestedQueries) {
-            	System.out.println(sugQuery);
+            int num = suggestedQueries.size();
+            System.out.println("suggested queries number: " + num);
+            System.out.println("top-5 suggested queries: ");
+            num = num < 5 ? num : 5;
+            for (int i = 0; i < num; i++) {
+            	System.out.println(suggestedQueries.get(i));
     		}
         }
-        resolver.close();
+        System.out.println("resolve time: " + ((new Date()).getTime() - date2.getTime()) + "ms");
     }
 } 
